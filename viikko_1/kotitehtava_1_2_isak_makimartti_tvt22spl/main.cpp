52edd4dfb00460abb621f32de5702b59832b4cca
Isak Makimartti <t2mais00@students.oamk.fi>
2023-01-12 11:01:47 +0200

    Viikon 1 Tehtävät

--- viikko_1/kotitehtava_1_2_isak_makimartti_tvt22spl/main.cpp
+++ viikko_1/kotitehtava_1_2_isak_makimartti_tvt22spl/main.cpp
@@ -8,39 +8,45 @@ int game(int maxnum);
 
 int main()
 {
+    int maxnumber, numberOfGuesses;
+
+    cout<<"Give maximum random number"<<endl;
+    cin>>maxnumber;
+
+    numberOfGuesses = game(maxnumber);
+    cout << "You guessed " << numberOfGuesses <<" times"<< endl;
+
     return 0;
 }
 
-int game(int maxnum){
+int game(int maxnum)
+{
     int input, rnum, guesses;
+
+    guesses = 1;
 
     srand (time(NULL));
     rnum = rand() % maxnum + 1;
-
-    cout<< "Give biggest possible number for random number generator" << endl;
-    cin>>maxnum;
 
     cout << "Guess a number between 1-" << maxnum << endl;
     cin>>input;
 
     while (input != rnum){
 
+    guesses++;
+
     if (input > rnum) {
         cout << "Your number is too big" << endl;
-        cout << "Guess again 1-" << maxnum<< endl;
+        cout << "Guess again: Number between 1-" << maxnum<< endl;
         cin>>input;
     }
     else              {
         cout << "Your number is too small" << endl;
-        cout << "Guess again 1-" << maxnum << endl;
+        cout << "Guess again: Number between 1-" << maxnum << endl;
         cin>>input;
     }
   }
-    if (input = rnum){
-        cout << "Correct!" << endl;
-    }
-
-    guesses = input++;
+       cout << "Correct! The number was: " << input << endl;
 
     return guesses;
 }
